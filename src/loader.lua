c50e9b69d2758ba664d63c523a522ba596a78522
ReactorCoreDev <165254059+ReactorCoreDev@users.noreply.github.com>
2025-02-11 22:15:28 +0100

    Update loader.lua

--- src/loader.lua
+++ src/loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/ReactorCoreDev/backdoor.exe-fixed-/blob/v8/src/main.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ReactorCoreDev/backdoor.exe/refs/heads/v8/src/main.lua"))();
