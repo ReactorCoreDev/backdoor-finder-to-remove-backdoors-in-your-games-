8594621af9f350ed0ad2516cd9bb5ec6a11829cb
ReactorCoreDev <165254059+ReactorCoreDev@users.noreply.github.com>
2025-02-11 22:21:38 +0100

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -469,4 +469,4 @@ ui.title.Text = TITLE;
 
 alertLib.Success(screenGui, TITLE, 'Backdoor scanner successfully loaded.');
 alertLib.Info(screenGui, TITLE, 'Home to toggle ui.', 4);
-alertLib.Info(screenGui, TITLE, 'This has been recontinued by ReactorCoreDev', 4);
+alertLib.Info(screenGui, TITLE, 'Rrecontinued by ReactorCoreDev!!', 4);
